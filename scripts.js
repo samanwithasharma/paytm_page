323ea8b91ece0504cfc73cbfe23c9213f25a9195
samanwithasharma <bgsamanwitha.05@gmail.com>
2025-01-16 21:35:52 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,139 @@
+
+var myButton   = document.querySelector(".learnBtn");
+var text2toggle = document.querySelector(".non_visible_text");
+
+myButton.addEventListener("click", function() {
+  if ((myButton.textContent === "+")) {
+    text2toggle.style.display = "flex";
+    myButton.textContent = "x";
+  } else {
+    text2toggle.style.display = "none";
+    myButton.textContent = "+";
+  }
+});
+/*More About paytm*/ 
+var myButton1   = document.querySelector(".learnBtn1");
+var text3toggle = document.querySelector(".non_visible_text0");
+
+myButton1.addEventListener("click", function() {
+  if ((myButton1.textContent === "+")) {
+    text3toggle.style.display = "flex";
+    myButton1.textContent = "x";
+  } else {
+    text3toggle.style.display = "none";
+    myButton1.textContent = "+";
+  }
+});
+
+var myButton2   = document.querySelector(".learnBtn2");
+var text4toggle = document.querySelector(".non_visible_text2");
+
+myButton2.addEventListener("click", function() {
+  if ((myButton2.textContent === "+")) {
+    text4toggle.style.display = "flex";
+    myButton2.textContent = "x";
+  } else {
+    text4toggle.style.display = "none";
+    myButton2.textContent = "+";
+  }
+});
+
+var myButton3   = document.querySelector(".learnBtn3");
+var text5toggle = document.querySelector(".non_visible_text3");
+
+myButton3.addEventListener("click", function() {
+  if ((myButton3.textContent === "+")) {
+    text5toggle.style.display = "flex";
+    myButton3.textContent = "x";
+  } else {
+    text5toggle.style.display = "none";
+    myButton3.textContent = "+";
+  }
+});
+
+/*Recharge & Pay bills*/ 
+var myButton4   = document.querySelector(".learnBtn4");
+var text6toggle = document.querySelector(".non_visible_text4");
+
+myButton4.addEventListener("click", function() {
+  if ((myButton4.textContent === "+")) {
+    text6toggle.style.display = "flex";
+    myButton4.textContent = "x";
+  } else {
+    text6toggle.style.display = "none";
+    myButton4.textContent = "+";
+  }
+});
+
+/*Pay Loan EMI*/ 
+var myButton5   = document.querySelector(".learnBtn5");
+var text7toggle = document.querySelector(".non_visible_text5");
+
+myButton5.addEventListener("click", function() {
+  if ((myButton5.textContent === "+")) {
+    text7toggle.style.display = "flex";
+    myButton5.textContent = "x";
+  } else {
+    text7toggle.style.display = "none";
+    myButton5.textContent = "+";
+  }
+});
+
+/*Book Travel & Entertainment*/ 
+var myButton6  = document.querySelector(".learnBtn6");
+var text8toggle = document.querySelector(".non_visible_text6");
+
+myButton6.addEventListener("click", function() {
+  if ((myButton6.textContent === "+")) {
+    text8toggle.style.display = "flex";
+    myButton6.textContent = "x";
+  } else {
+    text8toggle.style.display = "none";
+    myButton6.textContent = "+";
+  }
+});
+
+/*Investments & Miscellaneous*/ 
+var myButton7  = document.querySelector(".learnBtn7");
+var text9toggle = document.querySelector(".non_visible_text7");
+
+myButton7.addEventListener("click", function() {
+  if ((myButton7.textContent === "+")) {
+    text9toggle.style.display = "flex";
+    myButton7.textContent = "x";
+  } else {
+    text9toggle.style.display = "none";
+    myButton7.textContent = "+";
+  }
+});
+
+/*Loans and Credit Cards*/ 
+var myButton8  = document.querySelector(".learnBtn8");
+var text10toggle = document.querySelector(".non_visible_text8");
+
+myButton8.addEventListener("click", function() {
+  if ((myButton8.textContent === "+")) {
+    text10toggle.style.display = "flex";
+    myButton8.textContent = "x";
+  } else {
+    text10toggle.style.display = "none";
+    myButton8.textContent = "+";
+  }
+});
+
+/*Financial Tools & Calculators*/ 
+var myButton9 = document.querySelector(".learnBtn9");
+var text11toggle = document.querySelector(".non_visible_text9");
+
+myButton9.addEventListener("click", function() {
+  if ((myButton9.textContent === "+")) {
+    text11toggle.style.display = "flex";
+    myButton9.textContent = "x";
+  } else {
+    text11toggle.style.display = "none";
+    myButton9.textContent = "+";
+  }
+});
+
+
+
